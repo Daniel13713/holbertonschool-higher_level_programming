40dde714cd374a4b381f1fffd79d48652eedc35f
Daniel <danydu1299@gmail.com>
2021-12-21 14:42:06 -0500

    print number of two digits

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    print("{0}{1}".format(i // 10, i % 10), end="")
     if i != 99:
-        print(", ", end="")
+        print("{0}{1}".format(i // 10, i % 10), end=", ")
+    else:
+        print("99")
