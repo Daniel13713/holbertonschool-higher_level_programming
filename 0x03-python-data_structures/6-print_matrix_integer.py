204b6fa65deb6b02085064ffbf57da5b1b1e6780
daniel <danydu1299@gmail.com>
2022-01-03 14:23:57 -0500

    print a matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if matrix is not None:
+        for i in matrix:
+            sep = " "
+            for j in i:
+                print("{}".format(j), end=sep)
+                if i.index(j) + 2 == len(i):
+                    sep = "\n"
+    return (0)
