9e65884c6cf98ef0f158a66066d2799626cab31e
daniel <danydu1299@gmail.com>
2022-01-03 17:36:41 -0500

    point six

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,10 +2,4 @@
 def print_matrix_integer(matrix=[[]]):
     if matrix is not None:
         for i in matrix:
-            sep = " "
-            if i is not None:
-                for j in i:
-                    print("{:d}".format(j), end=sep)
-                    if i.index(j) + 2 == len(i):
-                        sep = ""
-                print("{}".format(""))
+            print(" ".join("{:d}".format(j) for j in i))
