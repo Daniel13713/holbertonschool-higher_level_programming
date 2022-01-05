e24df81741aec9764249e4a5a5033aa24d5dfe33
daniel <danydu1299@gmail.com>
2022-01-05 12:34:40 -0500

    change to symetric difference ^

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -2,5 +2,4 @@
 
 
 def common_elements(set_1, set_2):
-    if set_2 and set_1 is not None:
-        return set_1 & set_2
+    return set_1 & set_2
--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -2,5 +2,4 @@
 
 
 def only_diff_elements(set_1, set_2):
-    if set_1 and set_2 is not None:
-        return set_1 | set_2
+    return set_1 ^ set_2
