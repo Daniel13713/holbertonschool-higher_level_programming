9295404b1d7437b0a2f817348ce121fbb9be7b88
daniel <danydu1299@gmail.com>
2022-01-05 18:05:22 -0500

    calculate an average

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -6,6 +6,6 @@ def weight_average(my_list=[]):
     if my_list is not None:
         suma = list(map(lambda x: x[0] * x[1], my_list))
         total = [x[1] for x in my_list]
-        if total != 0:
+        if sum(total) != 0:
             result = sum(suma) / sum(total)
     return result
