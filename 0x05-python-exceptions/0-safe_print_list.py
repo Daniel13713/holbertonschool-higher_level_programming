178993a51ca31b5e1b0332607d0352fc3b8c453e
daniel <danydu1299@gmail.com>
2022-01-17 12:36:13 -0500

    solve the problem with the 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,9 +6,10 @@ def safe_print_list(my_list=[], x=0):
     try:
         for num in range(x):
             print("{}".format(my_list[num]), end="")
+        print("")
+        if num > 0:
+            num += 1
+        return num
     except IndexError:
         print("")
         return num
-    else:
-        print("")
-        return num + 1
