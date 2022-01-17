446abbe58f4347ef451c9ab9cb660c7923680b29
daniel <danydu1299@gmail.com>
2022-01-17 16:21:15 -0500

    safe integer error

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+    except Exception as error:
+        print("Exception:", error, file=sys.stderr)
+        isRigth = False
+    else:
+        isRigth = True
+    finally:
+        return isRigth
