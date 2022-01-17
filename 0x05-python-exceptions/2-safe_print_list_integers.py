7c5df91a28925b92de9ce035e837b0731cf3ed4a
daniel <danydu1299@gmail.com>
2022-01-17 14:42:44 -0500

    try with try in for

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,17 +2,15 @@
 
 
 def safe_print_list_integers(my_list=[], x=0):
-    num = 0
-    try:
-        for elem in range(x):
-            try:
-                print("{:d}".format(my_list[elem]), end="")
-            except (ValueError, TypeError):
-                pass
-            else:
-                num += 1
-        print("")
-        return num
-
-    except (IndexError) as Error:
-        print(Error)
+    num = x
+    for elem in range(x):
+        try:
+            print("{:d}".format(my_list[elem]), end="")
+        except (ValueError):
+            num -= 1
+            pass
+        except (IndexError) as Error:
+            print(Error)
+            return num
+    print("")
+    return num
