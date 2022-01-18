db426dc1d467844b0ff1313f25af1060e51f5a42
daniel <danydu1299@gmail.com>
2022-01-18 17:26:05 -0500

    verify the type of a varible in the class

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Class Square definition"""
+
+
+class Square:
+    """Class Square that define a Square"""
+
+    def __init__(self, size=0):
+        """Initilize a square
+        Attributtes:
+            - size (Private): size of the square
+
+        First determinate if the value of the size is right,
+         integer and positive
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
