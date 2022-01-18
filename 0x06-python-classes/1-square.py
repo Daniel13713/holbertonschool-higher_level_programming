6c69674bb68b078f1a7ed278af32d7093dd238ee
daniel <danydu1299@gmail.com>
2022-01-18 17:05:22 -0500

    private attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Class Square definition"""
+
+
+class Square:
+    """Class Square that define a Square"""
+
+    def __init__(self, size):
+        """Initilize a square
+        Attributtes:
+            - size (Private): size of the square
+        """
+        self.__size = size
