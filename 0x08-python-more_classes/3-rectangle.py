f28b1302b5bd91c1c3c6a6ce785ddb1e66ae72df
daniel <danydu1299@gmail.com>
2022-01-24 16:19:14 -0500

    task

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+"""Class Rectangle definition"""
+
+
+class Rectangle:
+    """Class Rectangle without attributes
+    Attirbutes:
+        - getter, setter width(int)
+        - getter and setter height(int)"""
+
+    def __init__(self, width=0, height=0):
+
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """getter of width, show the value"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """setter of the height: change the value of the height"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """getter of height, show the value"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """setter of the width: change the value of the width"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        """Public instance method: calculate the area of rectangle"""
+        return self.width * self.height
+
+    def perimeter(self):
+        """Public method that calculate the perimeter of rectangle"""
+        if self.width == 0 or self.height == 0:
+            return 0
+        else:
+            return (self.width + self.height) * 2
+
+    def __str__(self):
+        """method that return a string representation to user"""
+        msg = ""
+        if self.width == 0 or self.height == 0:
+            msg = ""
+        else:
+            msg += (("#" * self.width) + "\n") * self.height
+        return msg
--- 0x08-python-more_classes/README.md
+++ 0x08-python-more_classes/README.md
@@ -15,3 +15,25 @@
 ## Tasks
 #### 0. Simple rectangle
 Write an empty class Rectangle that defines a rectangle:
+
+#### 1. Real definition of a rectangle
+Private instance attribute: width:
+property **def width(self):** to retrieve it
+property setter **def width(self, value):** to set it:
+width must be an integer, otherwise raise a **TypeError** exception with the message **width must be an integer**
+if width is less than 0, raise a **ValueError** exception with the message **width must be >= 0**
+Private instance attribute: height:
+property **def height(self):** to retrieve it
+property setter**def height(self, value):** to set it:
+height must be an integer, otherwise raise a **TypeError** exception with the message **height must be an integer**
+if height is less than 0, raise a **ValueError** exception with the message **height must be >= 0**
+Instantiation with optional width and height: **def __init__(self, width=0, height=0):**
+
+#### 2. Area and Perimeter
+Public instance method: **def area(self):** that returns the rectangle area
+Public instance method: **def perimeter(self):** that returns the rectangle perimeter:
+if width or height is equal to 0, perimeter is equal to 0
+
+#### 3. String representation
+print() and str() should print the rectangle with the character #: (see example below)
+if width or height is equal to 0, return an empty string
