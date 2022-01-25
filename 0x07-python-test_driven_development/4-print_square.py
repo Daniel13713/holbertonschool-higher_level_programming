0a46775a2e05b13bae38e5fc4485cb72183c4317
daniel <danydu1299@gmail.com>
2022-01-25 16:21:49 -0500

    test cases task4 print_square, 9 cases, stable version

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""This module print a square
+    the size must be an integer
+    the function return nothing
+    try with print_square(1)
+    obtain: #"""
+
+
+def print_square(size):
+    """that prints in stdout the square with the character #:
+    Size must be an integer
+    Return: nothing"""
+    if type(size) != int:  #: if not isintance(size, int)
+        raise TypeError("size must be an integer")
+    elif size < 0:
+        raise ValueError("size must be >= 0")
+    elif size == 0:
+        print("")
+    else:
+        for y in range(size):
+            print("#" * size)
