4f1f9174d752d055256bcf3282938257e7be7363
daniel <danydu1299@gmail.com>
2022-02-01 18:43:34 -0500

    read a file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module read a teax file"""
+
+
+def read_file(filename=""):
+    """read a file with with and UTF8"""
+
+    with open(filename, encoding="utf-8") as file:
+        read_data = file.read()
+        print(read_data)
