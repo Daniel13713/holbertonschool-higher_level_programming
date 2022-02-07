db610e825daa2271d8885ca130844849dd6ff614
daniel <danydu1299@gmail.com>
2022-02-07 13:32:45 -0500

    :construction: :arrows_counterclockwise: serialization

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -22,9 +22,24 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """Json representation string"""
 
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        writes the JSON string representation of list_objs to a file:
+        """
+        if list_objs is None or len(list_objs) == 0:
+            list_dict = []
+        else:
+            list_dict = [obj.to_dictionary() for obj in list_objs]
+        filename = cls.__name__ + ".json"
+        with open(filename, mode="w", encoding="utf-8") as file:
+            obj_json = cls.to_json_string(list_dict)
+            file.write(obj_json)
