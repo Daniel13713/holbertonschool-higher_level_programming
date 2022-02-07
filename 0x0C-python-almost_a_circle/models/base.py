11f287e001f18693052d1d83f8a4a6a7fb3d1c56
daniel <danydu1299@gmail.com>
2022-02-07 15:32:01 -0500

    style

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -26,11 +26,7 @@ class Base:
     def to_json_string(list_dictionaries):
         """Json representation string"""
 
-        if (
-            list_dictionaries is None
-            or len(list_dictionaries) == 0
-            or list_dictionaries[0] is None
-        ):
+        if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
 
