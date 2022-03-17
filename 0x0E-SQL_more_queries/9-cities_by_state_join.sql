39f658fc0634785d3e6677f4dad6cc4e76bd9b23
Daniel <danydu1299@gmail.com>
2022-03-16 17:25:28 -0700

    try if the check allow chenge the name of the columns of the table joined

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -2,9 +2,9 @@
 -- Each record should display: cities.id - cities.name - states.name
 -- Results must be sorted in ascending order by cities.id
 -- You can use only one SELECT statement
-SELECT cities.id, cities.name, states.name
+SELECT cities.id, cities.name AS 'City', states.name AS 'State'
 FROM
-	cities 
+	cities
 	INNER JOIN
 	states
 	ON cities.state_id = states.id;
