1feb94e9fa3e5d97200854a372cf7efd4b1ce80e
daniel <danydu1299@gmail.com>
2022-04-05 23:04:58 -0500

    working with dictionaries

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const dict = require('./101-data.js').dict;
+const ocurrences = Object.keys(dict);
+const id = [...new Set(Object.values(dict))];
+const new_dict = {};
+for (const key of id) {
+  new_dict[key] = ocurrences.filter((e) => dict[e] === key);
+}
+console.log(dict);
+console.log(new_dict);
