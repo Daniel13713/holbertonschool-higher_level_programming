213b5cf0d4b08ffaae30cbad152422303618d0ef
Daniel1375 <danydu1299@gmail.com>
2022-04-12 09:55:47 -0500

    fixing style

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,12 +3,10 @@
 const fs = require('fs');
 
 const args = process.argv;
-const file = './' + args[2];
+const file = args[2];
 const content = args[3];
 fs.writeFile(file, content, { flag: 'a' }, err => {
   if (err) {
     console.log(err);
-    return;
   }
-  /* Written fine */
 });
