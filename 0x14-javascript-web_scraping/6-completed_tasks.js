823d822e5448ee32ca935aafa8ffb6f0e4e6a3ae
Daniel1375 <danydu1299@gmail.com>
2022-04-12 17:52:20 -0500

    using array methos to parse the data, i used: map, filter, and reduce. fixing something

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -13,10 +13,10 @@ request.get(URL, (error, response, body) => {
   const data = JSON.parse(body);
 
   const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  const completed = (user) => user.completed;
-  const usersCompleted = data.filter(completed);
   const forId = arr.map((id) => {
-    return usersCompleted.filter((user) => user.userId === id).length;
+    return data.filter((user) => {
+      return user.userId === id && user.completed;
+    }).length;
   }).reduce((total, value, index) => {
     return {
       ...total,
