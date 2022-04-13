1b31ef584de7d1aaa7f0c2ddd174012ec56fd0d1
Daniel1375 <danydu1299@gmail.com>
2022-04-12 23:08:57 -0500

    usando una función aparte para que el programa sea más retrasado

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -5,20 +5,28 @@ const args = process.argv;
 
 const URL = 'https://swapi-api.hbtn.io/api/films/' + args[2];
 
+/* Make the request to the film */
 request.get(URL, (error, response, body) => {
   if (error) {
     console.error('error:', error);
   }
   const data = JSON.parse(body);
-
-  data.characters.forEach(async (person) => {
-    const result = request.get(person, (error1, response1, body1) => {
-      if (error1) {
-        console.error('error:', error1);
-      }
-      const OneCharacter = JSON.parse(body1);
-      console.log(OneCharacter.name);
-    });
-    return result;
-  });
+  /* Call display with the character's arrays */
+  display(data.characters, 0);
 });
+
+/* request to the each character and display */
+function display (person, index) {
+  if (!person[index]) {
+    return;
+  }
+  request.get(person[index], (error, response, body) => {
+    if (error) {
+      console.error('error:', error);
+    }
+    const OneCharacter = JSON.parse(body);
+    console.log(OneCharacter.name);
+    display(person, ++index);
+  });
+  return 0;
+}
