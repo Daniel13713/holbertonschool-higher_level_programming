934ab043f92506335983f7e78b6ed398f36f938d
Daniel1375 <danydu1299@gmail.com>
2022-05-10 18:08:17 -0500

    creating city model and list all data

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Create a model City with sqlalchemy
+"""
+
+from sqlalchemy import Column, Integer, String, ForeignKeyConstraint
+from model_state import Base, State
+
+
+class City(Base):
+    """
+    Model State inherit from Base
+    """
+    __tablename__ = 'cities'
+    id = Column(
+        Integer,
+        primary_key=True,
+        autoincrement=True,
+        nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, nullable=False)
+
+    __table_args__ = (
+        ForeignKeyConstraint(['state_id'], ['states.id']),
+    )
