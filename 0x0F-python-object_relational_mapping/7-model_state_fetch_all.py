40c0da7514e18f3d6a38e4516c617c3ff8e8f68b
Daniel1375 <danydu1299@gmail.com>
2022-05-10 16:02:41 -0500

    Using session

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-"""List a states table"""
+"""
+    List all object of a tables(class)
+"""
 import sys
 from model_state import Base, State
-from sqlalchemy import (create_engine)
-
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
 
 if __name__ == "__main__":
-    """Don't execute when is imported"""
     engine = create_engine(
         'mysql+mysqldb://{}:{}@localhost/{}'.format(
             sys.argv[1],
@@ -15,6 +16,8 @@ if __name__ == "__main__":
         pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
-    data = engine.execute("SELECT * FROM states ORDER BY states.id;")
+    session = Session(engine)
+    data = session.query(State).order_by(State.id).all()
     for state in data:
         print("{}: {}".format(state.id, state.name))
+    session.close()
