42ce2367a3c11caf051eed8662f51ac6c0ee6ee7
Daniel1375 <danydu1299@gmail.com>
2022-05-10 15:38:34 -0500

    featch all data from a table

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -17,8 +17,6 @@ if __name__ == "__main__":
     """Base.metadata.create_all(engine)"""
 
     session = Session(engine)
-    data = session.query(State).order_by(State.id).all()
-
-    [print("{0}: {1}".format(state.id, state.name)) for state in data]
-
+    for state in session.query(State).order_by(State.id).all():
+        print("{}: {}".format(state.id, state.name))
     session.close()
