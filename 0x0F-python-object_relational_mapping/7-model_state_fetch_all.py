7a4a7b3401e0a9a90aa05fe75b24ffb539abeee6
Daniel1375 <danydu1299@gmail.com>
2022-05-10 15:47:42 -0500

    featch all data from a table

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 """Start link class to table in database
 """
+import sys
+from model_state import Base, State
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
 
 if __name__ == "__main__":
-    import sys
-    from model_state import Base, State
-
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import Session
-
     engine = create_engine(
         'mysql+mysqldb://{}:{}@localhost/{}'.format(
             sys.argv[1],
@@ -17,7 +16,12 @@ if __name__ == "__main__":
         pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
+    """
     session = Session(engine)
-    for state in session.query(State).order_by(State.id).all():
+    data = session.query(State).order_by(State.id).all()
+    """
+    query = "SELECT * FROM states ORDER BY states.id"
+    data = engine.execute(query)
+    for state in data:
         print("{}: {}".format(state.id, state.name))
-    session.close()
+    """session.close()"""
