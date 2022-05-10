39347c09d8faa30182772cf978ae55cb0c99b6bb
Daniel1375 <danydu1299@gmail.com>
2022-05-10 15:53:21 -0500

    Using execute for what???, comment the code with requirements

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
-"""Start link class to table in database
+"""
+    List a states table
 """
 import sys
 from model_state import Base, State
 from sqlalchemy import create_engine
 
 if __name__ == "__main__":
+    """
+        Don't execute when is imported
+    """
     engine = create_engine(
         'mysql+mysqldb://{}:{}@localhost/{}'.format(
             sys.argv[1],
@@ -14,12 +18,16 @@ if __name__ == "__main__":
         pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
-    """
-    session = Session(engine)
-    data = session.query(State).order_by(State.id).all()
-    """
     query = "SELECT * FROM states ORDER BY states.id"
     data = engine.execute(query)
     for state in data:
         print("{}: {}".format(state.id, state.name))
-    """session.close()"""
+
+    """
+    ANOTHER OPTION (not let by hokberton)
+    session = Session(engine)
+    data = session.query(State).order_by(State.id).all()
+    for state in data:
+        print("{}: {}".format(state.id, state.name))
+    session.close()
+    """
