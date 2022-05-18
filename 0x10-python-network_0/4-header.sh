1f5a6d9632ff53d08694261cf84b889bfb269f3e
Daniel1375 <danydu1299@gmail.com>
2022-05-18 17:00:03 -0500

    Add header variable in the request GET

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Write a Bash script that takes in a URL, sends a request to that URL
-curl -siH 'X-HolbertonSchool-User-Id: 98' "$1" | tail -1
+curl -siH 'X-HolbertonSchool-User-Id: 98' "$1" | tail -1 | awk 'printf "%s %s", $1,$3'
