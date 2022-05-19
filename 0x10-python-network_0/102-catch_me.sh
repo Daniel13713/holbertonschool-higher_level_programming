7f2d0b6b001afec2a7a68c1cb9999ed289beb83d
Daniel1375 <danydu1299@gmail.com>
2022-05-18 19:29:49 -0500

    PUT request

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Obtain only status code
-curl -sL -X PUT -d "user_id=98" -H "Origin: HolbertonSchool" "$1"
+curl -sLX PUT 0.0.0.0:5000/catch_me -d "user_id=98" -H "Origin: HolbertonSchool"
