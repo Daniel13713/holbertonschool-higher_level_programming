b203ea223fa86173f41e7e1b291272529634524e
Daniel1375 <danydu1299@gmail.com>
2022-05-19 22:43:12 -0500

    obtain headers, first request with Request method

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """What's my status?"""
 
-from urllib.request import urlopen
+from urllib.request import Request, urlopen
 from sys import argv
 
 
 URL = argv[1]
-with urlopen(URL) as response:
+req = Request(URL)
+with urlopen(req) as response:
     print(response.headers["X-Request-Id"])
