402cc738e1913505b78a1aac92f329d274148ef9
Daniel1375 <danydu1299@gmail.com>
2022-05-19 23:20:26 -0500

    Get request with requests module

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -3,8 +3,10 @@
 
 import requests
 
-URL = "https://intranet.hbtn.io/status"
-with requests.get(URL) as response:
-    print("Body response:")
-    print("\t- type: {}".format(type(response.text)))
-    print("\t- content: {}".format(response.text))
+if __name__ == "__main__":
+    URL = "https://intranet.hbtn.io/status"
+    with requests.session() as session:
+        response = session.get(URL)
+        print("Body response:")
+        print("\t- type: {}".format(type(response.text)))
+        print("\t- content: {}".format(response.text))
