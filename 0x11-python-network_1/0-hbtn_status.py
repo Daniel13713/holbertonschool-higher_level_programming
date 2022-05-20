3b2474bb7a80f9d9b6260b47c5ce0f244205961e
Daniel1375 <danydu1299@gmail.com>
2022-05-19 22:10:22 -0500

    obtain the status with urllin, using class bytes, decoding with .decode

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,6 +6,7 @@ from urllib.request import urlopen
 URL = "https://intranet.hbtn.io/status"
 with urlopen(URL) as response:
     content = response.read()
+    print("Body response:")
     print("\t- type: {}".format(type(content)))
     print("\t- content: {}".format(content))
     print("\t- utf8 content: {}".format(content.decode("utf-8")))
